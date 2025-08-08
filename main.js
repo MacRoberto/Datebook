4a1a45fba215ce3ad9dc4aaeae3fcfe29708d424
MacRoberto <roberto-may-c@outlook.es>
2025-08-08 01:43:21 -0500

    x1

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+function init() {
+  gapi.load("auth2", function () {
+    gapi.auth2.init({
+      client_id:
+        "462807780220-fdvduagjtlr4451st8co0288i9k2vmrn.apps.googleusercontent.com",
+      cookiepolicy: "single_host_origin",
+    });
+  });
+}
+
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   $("#name").text(profile.getName());
@@ -5,17 +15,13 @@ function onSignIn(googleUser) {
   $("#image").attr("src", profile.getImageUrl());
   $(".data").css("display", "block");
   $(".g-sining2").css("display", "none");
-  //console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  //console.log('Name: ' + profile.getName());
-  //console.log('Image URL: ' + profile.getImageUrl());
-  //console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
- function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      alert(You have been signed out successfully);
-        $(".g-sining2").css("display", "block");
-          $(".data").css("display", "none");
-    });
-  }
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    alert("You have been signed out successfully");
+    $(".g-sining2").css("display", "block");
+    $(".data").css("display", "none");
+  });
+}
